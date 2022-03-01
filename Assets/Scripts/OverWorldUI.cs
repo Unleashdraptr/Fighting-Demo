b7196622dd8ac1a2f6389f39d884740db21a80cb
Unleashdraptr <98326571+Unleashdraptr@users.noreply.github.com>
2022-03-01 19:33:05 +0000

    Overworld UI
    Added the base for the overworld UI

---
+++ Assets/Scripts/OverWorldUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OverWorldUI : MonoBehaviour
+{
+    public Text EnemiesLeftToKill;
+    public void OnPauseButton()
+    {
+
+    }
+}
